6c1cb12436ec6f4c44618721df0390cf5d7bd845
numver8638 <numver8638@naver.com>
2021-05-04 10:31:28 +0900

    Add datagen.py, dataset.py and fonts.

--- README.md
+++ README.md
@@ -27,3 +27,30 @@ This project (OCR_KorEng) is licensed under the MIT License. Also, various open 
 - [Pillow](https://python-pillow.org/) is licensed under the HPND License. Refer [here](https://github.com/python-pillow/Pillow/blob/master/LICENSE).
 - [OpenCV](https://opencv.org/) is licensed under the Apache License 2.0. (4.5.0 and higher)
 - [FreeType](https://www.freetype.org/) is licensed under the FreeType License. Refer [here](https://gitlab.freedesktop.org/freetype/freetype/-/blob/master/docs/FTL.TXT).
+
+### Used Fonts
+| Font File            | Font Name (English) | Font Name (Korean) | Author                        | License         | From       |
+|----------------------|---------------------|--------------------|-------------------------------|-----------------|------------|
+| KCCChassam.ttf       | KCCChassam          | KCC차쌤체          | 한국저작권위원회              | OFL             | 공유마당** |
+| KCCImkwontack.ttf    | KCCImkwontack       | KCC임권택체        | 한국저작권위원회              | OFL             | 공유마당** |
+| KoPubWorldDotum.ttf  | Kopub World Dotum   | Kopub World 돋움체 | 문화체육관광부, 한국출판회의  | 공공누리 1유형* | 공유마당** |
+| GyenggiTitle.ttf     | Gyenggi Title       | 경기천년제목체     | 경기도                        | 공공누리 1유형* | 공유마당** |
+| SeoulNamsan.ttf      | Seoul Namsan        | 서울남산체         | 서울시                        | 공공누리 1유형* | 공유마당** |
+| SeoulHangang.ttf     | Seoul Hangang       | 서울한강체         | 서울시                        | 공공누리 1유형* | 공유마당** |
+| JejuGothic.ttf       | Jeju Gothic         | 제주고딕체         | 제주시                        | 공공누리 1유형* | 공유마당** |
+| JejuMyeongjo.ttf     | Jeju Myeongjo       | 제주명조체         | 제주시                        | 공공누리 1유형* | 공유마당** |
+| PureunJeonnam.ttf    | Pureun Jeonnam      | 푸른전남체         | 전라남도                      | 공공누리 1유형* | 공유마당** |
+| NanumGothic.ttf      | Nanum Gothic        | 나눔고딕           | (주)네이버, 재 네이버문화재단 | OFL             | 공유마당** |
+| NanumBarunGothic.ttf | Nanum Barun Gothic  | 나눔바른고딕       | (주)네이버, 재 네이버문화재단 | OFL             | 공유마당** |
+| NanumBarunpen.ttf    | Nanum Barun Pen     | 나눔바른펜         | (주)네이버, 재 네이버문화재단 | OFL             | 공유마당** |
+| NanumPenScript.ttf   | Nanum Pen Script    | 나눔손글씨 붓체    | (주)네이버, 재 네이버문화재단 | OFL             | 공유마당** |
+| NanumBrushScript.ttf | Nanum Brush Script  | 나눔손글씨 펜체    | (주)네이버, 재 네이버문화재단 | OFL             | 공유마당** |
+| KCCDodamdodam.ttf    | KCC Dodamdodam      | KCC 도담도담체     | 한국저작권위원회              | OFL             | 공유마당** |
+
+### Reference
+\* 공공누리 1유형: [여기](https://www.kogl.or.kr/info/license.do#01-tab)를 참고하세요.  
+\*\* 공유마당 : https://gongu.copyright.or.kr/gongu/bbs/B0000018/list.do?menuNo=200195
+
+### Reference (English)
+\* 공공누리 1유형(KOGL - Korean Open Goverment License, "Gonggongnuri" Type 1): Please refer [here](https://www.kogl.or.kr/info/license.do#05-tab) to get more information.  
+\*\* 공유마당 : https://gongu.copyright.or.kr/gongu/bbs/B0000018/list.do?menuNo=200195
---
+++ dataset.py
@@ -0,0 +1,167 @@
+#
+# dataset.py
+# - Serialize and deserialize dataset.
+#
+from os.path import join, dirname
+import tensorflow as tf
+import glob
+import numpy as np
+
+
+# Copied from tensorflow documentation.
+def _int64_feature(value):
+    """Wrapper for inserting int64 features into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def _float_feature(value):
+    """Wrapper for inserting float features into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def _floats_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def _bytes_feature(value):
+    """Wrapper for inserting bytes features into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def _count(iterable):
+    sum = 0
+    for begin, end in iterable:
+        sum += end - begin
+    return sum
+
+
+DATA_ROOT = join(dirname(__file__), 'dataset')
+FONT_ROOT = join(dirname(__file__), 'fonts')
+WIDTH = 32
+HEIGHT = 32
+
+# 0021 ~ 007E - Basic Latin
+# 3130 ~ 318F - Hangul Compatibility Jamo, except 3130, 318F(unassigned) and 3164(not printable)
+# AC00 ~ D7A3 - Hangul Syllables
+_CHAR_RANGES = (
+    (0x0021, 0x007F),
+    (0x3131, 0x3164),
+    (0x3165, 0x318F),
+    (0xAC00, 0xD7A4)
+)
+MAX_CHARS = _count(_CHAR_RANGES)
+
+
+def range_chars():
+    '''
+    Iterator for all characters used in dataset.
+    '''
+    for begin, end in _CHAR_RANGES:
+        for ch in range(begin, end):
+            yield chr(ch)
+
+
+# Table mapped index (int) to label (str).
+LABELS = {index : label for index, label in zip(range(MAX_CHARS), range_chars())}
+
+# Table mapped label (str) to index (int).
+INDEXES = {label : index for index, label in zip(range(MAX_CHARS), range_chars())}
+
+
+def serialize(image, label):
+    '''
+    Serialize image and label to tfrecord.
+
+    Args:
+        image (numpy.ndarray):
+        label (int):
+
+    Returns:
+        ... 
+    '''
+    sample, height, width, channel = image.shape
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'image/sample' : _int64_feature(sample),
+        'image/width' : _int64_feature(width),
+        'image/height' : _int64_feature(height),
+        'image/channel' : _int64_feature(channel),
+        'image/label' : _int64_feature(label),
+        'image/data' : _floats_feature(image.flat)
+    }))
+
+    return example.SerializeToString()
+
+
+_FEATURE_DESCRIPTION = {
+    'image/sample' : tf.io.FixedLenFeature([], tf.int64),
+    'image/width' : tf.io.FixedLenFeature([], tf.int64),
+    'image/height' : tf.io.FixedLenFeature([], tf.int64),
+    'image/channel' : tf.io.FixedLenFeature([], tf.int64),
+    'image/label' : tf.io.FixedLenFeature([], tf.int64),
+    'image/data' : tf.io.FixedLenSequenceFeature([], tf.float32, allow_missing=True)
+}
+
+
+def deserialize(raw_example):
+    '''
+    Deserialize image and label from string tensor.
+
+    Args:
+        raw_example: serialized tf.io.Example
+    
+    Returns:
+        tuple of image and label.
+    '''
+    example = tf.io.parse_single_example(raw_example, _FEATURE_DESCRIPTION)
+
+    sample = example['image/sample']
+    width = example['image/width']
+    height = example['image/height']
+    channel = example['image/channel']
+    label = example['image/label']
+    data = example['image/data']
+
+    return tf.reshape(data, (sample, width, height, channel)), label
+
+
+def _load(data_type):
+    files = glob.glob(join(DATA_ROOT, data_type, '*.tfrecord'))
+
+    return tf.data.TFRecordDataset(filenames=files, compression_type="GZIP").map(deserialize)
+
+
+def load_train():
+    '''
+    Load train dataset.
+
+    Returns:
+        tf.data.Dataset contains train dataset.
+    '''
+    return _load('train')
+    
+
+def load_validation():
+    '''
+    Load validation dataset.
+
+    Returns:
+        tf.data.Dataset contains validation dataset.
+    '''
+    return _load('validation')
+
+
+def load_test():
+    '''
+    Load test dataset.
+
+    Returns:
+        tf.data.Dataset contains test dataset.
+    '''
+    return _load('test')
