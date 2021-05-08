e023af9eac4d51f6663b73c242f50a8baa2eb357
numver8638 <numver8638@naver.com>
2021-05-09 02:35:14 +0900

    Create fit.sh and modify fit.py to save log and model separately.

--- fit.py
+++ fit.py
@@ -5,18 +5,20 @@
 from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras import layers, models, callbacks
-from os.path import join, dirname
+from os.path import dirname, exists, join
+import multiprocessing as mp
 
 import dataset
 
 PROJECT_ROOT = dirname(__file__)
-MODEL_ROOT = join(PROJECT_ROOT, 'model')
+MODELS_ROOT = join(PROJECT_ROOT, 'models')
 LOGS_ROOT = join(PROJECT_ROOT, 'logs')
 BATCH_SIZE = 16
 EPOCHS = 20
 
+def fit(folder_name):
+    hp = load_hyperparams()
 
-def fit():
     train_data = dataset.load_train().shuffle(1000).batch(BATCH_SIZE)
     validation_data = dataset.load_validation().shuffle(1000).batch(BATCH_SIZE)
 
@@ -41,12 +43,33 @@ def fit():
               verbose=2,
               callbacks=[
                   # Save log for tensorboard.
-                  callbacks.TensorBoard(log_dir=LOGS_ROOT, histogram_freq=1),
+                  callbacks.TensorBoard(log_dir=join(LOGS_ROOT, folder_name), histogram_freq=1),
                   # Prevent overfitting.
-                  callbacks.EarlyStopping()
-              ])
-    model.save(MODEL_ROOT)
+                  callbacks.EarlyStopping(monitor='val_accuracy', mode='max', verbose=1, patience=5),
+                  # Save checkpoint
+                  callbacks.ModelCheckpoint(join(MODELS_ROOT, folder_name), monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+                  # callbacks.LambdaCallback(on_epoch_end=log_confusion_matrix)
+              ],
+              workers=mp.cpu_count(),
+              use_multiprocessing=True)
+
+    test_data = dataset.load_test().shuffle(1000).batch(BATCH_SIZE)
+
+    test_acc, test_loss = model.evaluate(test_data,
+                                         verbose=2,
+                                         callbacks=[
+                                             callbacks.TensorBoard(log_dir=join(LOGS_ROOT, folder_name), histogram_freq=1)
+                                         ],
+                                         workers=mp.cpu_count(),
+                                         use_multiprocessing=True)
+
+    print("test_acc:", test_acc, "test_loss:", test_loss)
 
 
 if __name__ == '__main__':
-    fit()
+    import sys
+
+    if len(sys.argv) != 2:
+        print("usage: fit.py <folder_name>")
+    elif not exists(HYPERPARAMS_FILE):
+        fit(sys.argv[1])
